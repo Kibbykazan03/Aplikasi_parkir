ebd483eb54a3c6ea75e847f13d9b8fe2928aba8c
KibbyKazan03 <Kibbykazan@gmail.com>
2026-01-16 07:47:57 +0000

    Alur transaksi masuk parkiran

--- petugas/dashboard.php
+++ petugas/dashboard.php
@@ -1,6 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Petugas Parkir</title>
+</head>
+<body>
+
 <?php
 session_start();
 if ($_SESSION['role'] != 'petugas') {
     header("Location: ../auth/login.php");
 }
-echo "Dashboard Petugas";
+
+?>
+
+<h2>Dashboard Petugas</h2>
+
+<ul>
+    <li>
+        <a href="transaksi_masuk.php"> Transaksi Parkir Masuk </a>
+    </li>
+</ul>
+    
+</body>
+</html>
---
+++ petugas/transaksi_masuk.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Transaksi masuk</title>
+</head>
+
+<body>
+
+    <?php
+    session_start();
+    include "../config/koneksi.php";
+
+
+    if ($_SESSION['role'] != 'petugas') {
+        header("Location: ../auth/login.php");
+        exit;
+    }
+    ?>
+
+    <h2>Transaksi Parkir Masuk</h2>
+    <form method="POST">
+        plat nomor <br>
+        <input type="text" name="plat" required><br>
+
+        jenis kemdaraan <br>
+        <select name="jenis">
+            <option value="motor">Motor</option>
+            <option value="mobil">Mobil</option>
+        </select><br>
+
+        Area parkir <br>
+        <select name="area">
+
+            <?php
+
+            $area = mysqli_query($koneksi, "SELECT *FROM tb_area_parkir");
+            while ($a = mysqli_fetch_assoc($area)) {
+                echo "<option value='" . $a['id_area'] . "'>" . $a['nama_area'] . "</option>";
+            }
+            ?>
+        </select><br>
+
+        <button type="submit" name="simpan">Simpan</button>
+
+    </form>
+
+    <?php
+    if (isset($_POST['simpan'])) {
+
+        $plat  = $_POST['plat'];
+        $jenis = $_POST['jenis'];
+        $area  = $_POST['area'];
+
+        $cek = mysqli_query($koneksi, "
+    SELECT tb_transaksi.id_transaksi 
+    FROM tb_kendaraan 
+    JOIN tb_transaksi ON tb_kendaraan.id_kendaraan = tb_transaksi.id_kendaraan
+    WHERE tb_kendaraan.plat_nomor = '$plat'
+    AND tb_transaksi.status = 'masuk'
+");
+
+        if (mysqli_num_rows($cek) > 0) {
+            echo "<p style='color:red;'>❌ Kendaraan dengan plat ini masih parkir!</p>";
+            exit;
+        }
+
+
+        // simpan kendaraan
+        mysqli_query($koneksi, "INSERT INTO tb_kendaraan (plat_nomor, jenis_kendaraan)
+                            VALUES ('$plat', '$jenis')");
+
+        $id_kendaraan = mysqli_insert_id($koneksi);
+
+        // ambil tarif
+        $tarif = mysqli_query($koneksi, "SELECT * FROM tb_tarif WHERE jenis_kendaraan='$jenis'");
+        $t = mysqli_fetch_assoc($tarif);
+
+        // simpan transaksi
+        mysqli_query($koneksi, "INSERT INTO tb_transaksi 
+        (id_kendaraan, waktu_masuk, id_tarif, status, id_user, id_area)
+        VALUES
+        ('$id_kendaraan', NOW(), '" . $t['id_tarif'] . "', 'masuk', '" . $_SESSION['id_user'] . "', '$area')
+    ");
+
+        echo "<p>Transaksi masuk berhasil</p>";
+    }
+
+
+    ?>
+</body>
+
+</html>
