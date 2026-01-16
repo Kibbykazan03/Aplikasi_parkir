40ce9fa680cf228cae472dc4f6ef4bab40f7301f
KibbyKazan03 <Kibbykazan@gmail.com>
2026-01-16 11:58:05 +0000

    Tampilan awal aplikasi

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
 <?php
-echo "halooo tukang parkir"
-?>
+// ini gerbang awal saat halaman website di buka
+
+session_start();
+
+if (isset($_SESSION['role'])) {
+    // kalau sudah login
+    if ($_SESSION['role'] == 'admin') {
+        header("Location: admin/dashboard.php");
+    } elseif ($_SESSION['role'] == 'petugas') {
+        header("Location: petugas/dashboard.php");
+    }
+} else {
+    // kalau belum login
+    header("Location: auth/login.php");
+}
+exit;
